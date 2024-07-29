ab13c584e9b2c752d06124476da9b0d040ff662f
alantorbd <alabtorbd@gmail.com>
2024-07-30 03:42:39 +0600

    add some code

---
+++ 03_obj_array.js
@@ -0,0 +1,26 @@
+//arrays is reference type data it store into heep memory
+// let obj = {
+//   name: "Antor",
+//   "your age": 22,
+//   greet: () => "hello world!",
+// };
+// console.log(obj.greet());
+// console.log(obj["your age"]);
+// console.log(obj.name);
+// obj2 = { obj };
+// // obj2 = JSON.parse(JSON.stringify(obj));
+// obj2.name = "Joy";
+// console.log(obj.name);
+// console.log(obj2.name);
+// obj.city = "Kushtia";
+// console.log(obj);
+// delete obj.name;
+// console.log(obj);
+
+//------------------------array------------------------
+
+let arr = [1, 3, 4, 5, 6];
+console.log(arr);
+//arr constructor
+let arr1 = new Array(1, 2, 3, 4, 5);
+console.log(typeof arr1);
